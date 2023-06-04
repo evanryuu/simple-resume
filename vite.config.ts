23e2eaaeda003daedf52f324a1c2c106d7d67581
evankwolf <452367115@qq.com>
2023-06-05 02:09:48 +0800

    fix: fix build time print in console

--- vite.config.ts
+++ vite.config.ts
@@ -3,20 +3,21 @@ import path from 'path'
 import react from '@vitejs/plugin-react'
 import UnoCSS from 'unocss/vite'
 import { defineConfig } from 'vite'
+import { createHtmlPlugin } from 'vite-plugin-html'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/simple-resume/',
-  build: {
-    rollupOptions: {
-      output: {
-        intro: 'console.log(\'Build time:\', new Date().toLocaleString())',
-      },
-    },
-  },
   plugins: [
     react(),
     UnoCSS({ /* options */ }),
+    createHtmlPlugin({
+      inject: {
+        data: {
+          buildTimestamp: Date.now(),
+        },
+      },
+    }),
   ],
   resolve: {
     alias: {
